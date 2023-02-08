d0498cb6aeb72294baaa0e3c161b234b46aeeb65
lam <lambro25102001@gmail.com>
2023-02-08 14:39:53 +0700

    debug

--- src/main/java/com/salespage/salespageservice/domains/utils/EmailRequest.java
+++
@@ -1,9 +0,0 @@
-package com.salespage.salespageservice.domains.utils;
-
-import java.util.Properties;
-
-
-public class EmailRequest {
-  public static void sendVerificationCode(String to, String verificationCode) {
-  }
-}
