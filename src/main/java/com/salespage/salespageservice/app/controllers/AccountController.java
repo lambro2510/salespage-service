12137e539ce710020a831d4dbc3076addd296edd
Lam2001 <106224588+lambro2510@users.noreply.github.com>
2023-03-02 13:11:48 +0000

    tesst

--- src/main/java/com/salespage/salespageservice/app/controllers/AccountController.java
+++ src/main/java/com/salespage/salespageservice/app/controllers/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.io.IOException;
 
-@CrossOrigin(origins = "https://salepage-fontend-yygsn.appengine.bfcplatform.vn", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("v1/api/account")
 public class AccountController extends BaseController {
