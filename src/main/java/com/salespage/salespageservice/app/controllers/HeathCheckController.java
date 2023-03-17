461a2e66275be2270f0ccdb20d6a0e9629f70915
Lam2001 <106224588+lambro2510@users.noreply.github.com>
2023-03-17 16:30:21 +0700

    test

--- src/main/java/com/salespage/salespageservice/app/controllers/HeathCheckController.java
+++ src/main/java/com/salespage/salespageservice/app/controllers/HeathCheckController.java
@@ -1,27 +1,27 @@
-package com.salespage.salespageservice.app.controllers;
+// package com.salespage.salespageservice.app.controllers;
 
-import com.salespage.salespageservice.domains.datas.ExchangeMoney;
-import com.salespage.salespageservice.domains.utils.JsonParser;
-import com.salespage.salespageservice.domains.utils.RequestUtil;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import com.salespage.salespageservice.domains.datas.ExchangeMoney;
+// import com.salespage.salespageservice.domains.utils.JsonParser;
+// import com.salespage.salespageservice.domains.utils.RequestUtil;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-@CrossOrigin
-@RestController
-@RequestMapping("")
-public class HeathCheckController {
-    @Value("${url.exchange}")
-    private String ExchangeUrl;
+// import java.io.IOException;
+// @CrossOrigin
+// @RestController
+// @RequestMapping("")
+// public class HeathCheckController {
+//     @Value("${url.exchange}")
+//     private String ExchangeUrl;
 
-    @GetMapping("")
-    public ResponseEntity<?> checkExchangeMoney() throws IOException {
-        String data = RequestUtil.request(ExchangeUrl);
-        data = data.substring(data.indexOf("["), data.lastIndexOf("]") + 1);
-        return ResponseEntity.ok(JsonParser.arrayList(data, ExchangeMoney.class));
-    }
-}
+//     @GetMapping("")
+//     public ResponseEntity<?> checkExchangeMoney() throws IOException {
+//         String data = RequestUtil.request(ExchangeUrl);
+//         data = data.substring(data.indexOf("["), data.lastIndexOf("]") + 1);
+//         return ResponseEntity.ok(JsonParser.arrayList(data, ExchangeMoney.class));
+//     }
+// }
