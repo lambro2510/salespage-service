4acffa6bc66c626e6b200e9f56811a0eb60505ad
Lam2001 <106224588+lambro2510@users.noreply.github.com>
2023-03-17 12:59:49 +0700

    test

--- src/main/java/com/salespage/salespageservice/domains/config/SwaggerConfig.java
+++ src/main/java/com/salespage/salespageservice/domains/config/SwaggerConfig.java
@@ -1,22 +1,79 @@
 package com.salespage.salespageservice.domains.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.salespage.salespageservice.domains.Constants;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-   @Bean
-   public Docket api() {
-      return new Docket(DocumentationType.SWAGGER_2)
-         .select()
-         .apis(RequestHandlerSelectors.any())
-         .paths(PathSelectors.any())
-         .build();
-   }
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    TypeResolver typeResolver = new TypeResolver();
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .additionalModels(
+                        typeResolver.resolve(Long.class),
+                        typeResolver.resolve(Field.Str.class))
+                .apiInfo(apiEndPointsInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Sale page api")
+                .description("Lam salepage")
+                .contact(new Contact("Support - 0979163206", "/", "lambro25102001@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.1")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", Constants.AUTH_KEY, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
--- src/main/java/com/salespage/salespageservice/domains/security/WebSecurityConfig.java
+++ src/main/java/com/salespage/salespageservice/domains/security/WebSecurityConfig.java
@@ -62,7 +62,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
-                .antMatchers("/v1/api/account/**", "/v1/api/public/**")
+                .antMatchers("/v1/api/account/**", "/v1/api/public/**,",
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
                 .permitAll();
 
         http.authorizeRequests().anyRequest().authenticated();
