51045499dc5c5dd41cd3fbb415fb3b8ee8ffd53d
lam <lambro25102001@gmail.com>
2023-06-16 19:48:53 +0800

    fix swagger response

--- src/main/java/com/salespage/salespageservice/app/responses/swaggerResponse/BankListDataRes.java
+++ src/main/java/com/salespage/salespageservice/app/responses/swaggerResponse/BankListDataRes.java
@@ -3,5 +3,7 @@ package com.salespage.salespageservice.app.responses.swaggerResponse;
 import com.salespage.salespageservice.app.responses.BankResponse.BankListData;
 import com.salespage.salespageservice.app.responses.BaseResponse;
 
-public class BankListDataRes extends BaseResponse<BankListData> {
+import java.util.List;
+
+public class BankListDataRes extends BaseResponse<List<BankListData>> {
 }
