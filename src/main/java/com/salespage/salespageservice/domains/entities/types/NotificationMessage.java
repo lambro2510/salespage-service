bf995f85789495fdab17294c2dead81d0db3d586
lam <lambro25102001@gmail.com>
2023-06-19 23:09:37 +0800

    edit noti

--- src/main/java/com/salespage/salespageservice/domains/entities/types/NotificationMessage.java
+++ src/main/java/com/salespage/salespageservice/domains/entities/types/NotificationMessage.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum NotificationMessage {
-  CHANGE_STATUS_PAYMENT_PENDING("Giao dịch đã bị chuyển trạng thái", "Giao dịch đã bị chuyển về trạng thái chờ do không nhận được dữ liệu xử lý"),
+  CHANGE_STATUS_PAYMENT_PENDING("Giao dịch đã bị chuyển về trạng thái chờ", "Giao dịch đã bị chuyển về trạng thái chờ do đã quá hạn xử lý"),
   CHANGE_STATUS_PAYMENT_RESOLVE_IN("Giao dịch thành công", "Hệ thống đã nhận được yêu cầu chuyển tiền. Vui long chuyển tiền đến số tài khoản theo mã QR để hoàn tất quá trình chuyển tiền"),
   CHANGE_STATUS_PAYMENT_RESOLVE_OUT_ERR("Giao dịch không thành công", "Tài khoản của bạn không đủ tiền để rút."),
   CHANGE_STATUS_PAYMENT_RESOLVE_OUT("Giao dịch thành công", "Hệ thống đã nhận được yêu cầu chuyển tiền. Vui lòng chờ để hệ thống xử lý việc chuyển tiền "),
