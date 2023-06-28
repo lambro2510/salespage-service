5d33caa3a4f2f2ace498b069cf7c50a532b5df02
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-06-28 17:39:38 +0800

    schedule

--- src/main/java/com/salespage/salespageservice/app/schedules/StatisticSchedule.java
+++ src/main/java/com/salespage/salespageservice/app/schedules/StatisticSchedule.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @Log4j2
 public class StatisticSchedule {
@@ -15,36 +18,43 @@ public class StatisticSchedule {
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticToday(){
+        log.info("asyncTransactionStatisticToday -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticToday();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticPeriodDay(){
+        log.info("asyncTransactionStatisticPeriodDay -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticPeriodDate();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticWeek(){
+        log.info("asyncTransactionStatisticWeek -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticWeek();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticPeriodWeek(){
+        log.info("asyncTransactionStatisticPeriodWeek -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticPeriodWeek();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticMonth(){
+        log.info("asyncTransactionStatisticMonth -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticMonth();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticPeriodMonth(){
+        log.info("asyncTransactionStatisticPeriodMonth -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticPeriodMonth();
     }
 
     @Scheduled(fixedDelay = 1000 * 30) //30s 1 lần
     public void asyncTransactionStatisticYear(){
+        log.info("asyncTransactionStatisticYear -> {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss")));
         transactionStatisticService.statisticYear();
     }
 
