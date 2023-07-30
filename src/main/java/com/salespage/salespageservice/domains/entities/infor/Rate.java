7068a62d724ecd456e168230c1dbcd46159bb226
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-07-30 21:22:28 +0800

    fĩx l

--- src/main/java/com/salespage/salespageservice/domains/entities/infor/Rate.java
+++ src/main/java/com/salespage/salespageservice/domains/entities/infor/Rate.java
@@ -3,27 +3,29 @@ package com.salespage.salespageservice.domains.entities.infor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.math.BigDecimal;
+
 @Data
 public class Rate {
 
     @Field("total_point")
-    private Float totalPoint = 0F;
+    private Float totalPoint = 0F;  //Tổng điểm đánh giá
 
     @Field("total_rate")
-    private Float totalRate = 0F;
+    private Float totalRate = 0F;   //Số lượng đánh giá
 
     @Field("avg_point")
-    private Float avgPoint = 0F;
+    private Float avgPoint = 0F;    //Điểm đánh giá trung bình
 
     public void processAddRatePoint(Float point) {
-        totalRate += 1;
-        totalPoint += point;
-        avgPoint = totalPoint / totalRate;
+        this.totalRate += 1;
+        this.totalPoint += point;
+        this.avgPoint = BigDecimal.valueOf(totalPoint).divide(BigDecimal.valueOf(totalRate)).floatValue();
     }
 
     public void processUpdateRatePoint(Float oldPoint ,Float point) {
-        totalPoint = totalPoint - oldPoint + point;
-        avgPoint = totalPoint / totalRate;
+        this.totalPoint = this.totalPoint - oldPoint + point;
+        this.avgPoint = BigDecimal.valueOf(totalPoint).divide(BigDecimal.valueOf(totalRate)).floatValue();
 
     }
 }
