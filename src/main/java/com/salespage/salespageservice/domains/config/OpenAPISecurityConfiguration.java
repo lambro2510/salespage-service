28a5e21e76711ba8f41f1454cf48a136f9431aff
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-09-22 14:43:55 +0630

    swagger

--- src/main/java/com/salespage/salespageservice/domains/config/OpenAPISecurityConfiguration.java
+++ src/main/java/com/salespage/salespageservice/domains/config/OpenAPISecurityConfiguration.java
@@ -16,23 +16,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-    info =@Info(
-        title = "Salepage api",
-        version = "v1",
-        contact = @Contact(
-            name = "lambro2510", email = "lambro2510@gmail.com", url = "http://lam-banhang.click"
-        ),
-        license = @License(
-            name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
-        ),
-        description = "api mô tả"
-    )
-)
+//@OpenAPIDefinition(
+//    info =@Info(
+//        title = "Salepage api",
+//        version = "v1",
+//        contact = @Contact(
+//            name = "lambro2510", email = "lambro2510@gmail.com", url = "http://lam-banhang.click"
+//        ),
+//        license = @License(
+//            name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
+//        ),
+//        description = "api mô tả"
+//    )
+//)
 public class OpenAPISecurityConfiguration {
   @Bean
   public OpenAPI customizeOpenAPI() {
-    final String securitySchemeName = "auth";
+    final String securitySchemeName = "bearerAuth";
     return new OpenAPI()
         .addSecurityItem(new SecurityRequirement()
             .addList(securitySchemeName))
