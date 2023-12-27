24f73f1b8dcf24c365447065ffc365d7484c1cc6
lam <lambro2510@gmail.com>
2023-12-28 00:22:02 +0700

    fix ai réposne

--- src/main/java/com/salespage/salespageservice/app/responses/Status.java
+++ src/main/java/com/salespage/salespageservice/app/responses/Status.java
@@ -2,9 +2,11 @@ package com.salespage.salespageservice.app.responses;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public enum Status {
   VERY_GOOD(5),
   GOOD(4),
